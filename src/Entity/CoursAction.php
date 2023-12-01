ea58bebb5b33404fb25d7a90f4cf4a135cd8f3e7
Gorkeur <Alexandre.bar92fr@gmail.com>
2023-12-01 15:58:46 +0100

    creation classe CoursAction

---
+++ src/Entity/CoursAction.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CoursActionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CoursActionRepository::class)]
+class CoursAction
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateCoursAction = null;
+
+    #[ORM\Column]
+    private ?int $prix = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateCoursAction(): ?\DateTimeInterface
+    {
+        return $this->dateCoursAction;
+    }
+
+    public function setDateCoursAction(\DateTimeInterface $dateCoursAction): static
+    {
+        $this->dateCoursAction = $dateCoursAction;
+
+        return $this;
+    }
+
+    public function getPrix(): ?int
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(int $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+}
