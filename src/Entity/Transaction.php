9f86322ed23651dc79e768fcc71b5ca1929da4f3
Gorkeur <Alexandre.bar92fr@gmail.com>
2023-12-01 16:01:02 +0100

    creation classe transaction

---
+++ src/Entity/Transaction.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TransactionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TransactionRepository::class)]
+class Transaction
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateTransaction = null;
+
+    #[ORM\Column]
+    private ?int $quantite = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $operation = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateTransaction(): ?\DateTimeInterface
+    {
+        return $this->dateTransaction;
+    }
+
+    public function setDateTransaction(\DateTimeInterface $dateTransaction): static
+    {
+        $this->dateTransaction = $dateTransaction;
+
+        return $this;
+    }
+
+    public function getQuantite(): ?int
+    {
+        return $this->quantite;
+    }
+
+    public function setQuantite(int $quantite): static
+    {
+        $this->quantite = $quantite;
+
+        return $this;
+    }
+
+    public function getOperation(): ?string
+    {
+        return $this->operation;
+    }
+
+    public function setOperation(string $operation): static
+    {
+        $this->operation = $operation;
+
+        return $this;
+    }
+}
